5f0f0bcdb3199576dcabf60f76bde52555e1df42
Samantha Balgobin <sbalgobin94@gmail.com>
2020-07-21 16:45:08 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   if array.include?(value_to_find) == true
-    return array.index(value_tofind)
+    array.index(value_tofind)
   else
-    return NIL
+    return nil
   end
 end
+
